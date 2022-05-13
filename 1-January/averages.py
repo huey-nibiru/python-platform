dbec46a1ec937ee1259b4c3cdbb7f33ecdcc88cd
Yousef Macer <yousefmacer@gmail.com>
2022-05-13 14:40:44 -0400

    Update averages.py

--- 1-January/averages.py
+++ 1-January/averages.py
@@ -1,17 +1,32 @@
-mynumbers = []
-sum = 0
+# create a list that will store the numbers that are being averaged
+numbers = []
 
+
+# this while loop will let us keep adding new numbers into the list of numbers
 while True:
-  #by default input is a string
+  
+  # let the user input the number they want to average 
   number = input("Enter 'quit' to quit or enter a number >>> ")
+  
+  # if the user enters the word 'quit' the loop will stop and the average will show
   if number == "quit":
     break
-  number = int(number) # this turns it into a number
-  mynumbers.append(number)
-  sum += number
+  
+  # when the user enters a number, we will convert it from string to integer
+  # we then add each number to the list of numbers
+  else:
     
-average = sum / len(mynumbers)
-print("the average of these numbers is " + str(average))
-  
-  
-  
+    # if the user enters any letters, the computer will tell us to enter only numbers
+    try:
+      number = int(number) 
+      numbers.append(number)
+    except:
+      print("Please enter a number only.\n")
+
+
+# create the average by taking the sum of the list and divide by length of the list
+average = sum(numbers) / len(numbers)
+
+# display the outcome
+print("\nNumbers:", numbers)
+print("Average:", average)
